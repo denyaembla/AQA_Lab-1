b22888a790a063dfe8936e79433cdcb109e555e7
denyaembla <denisholder12@gmail.com>
2022-02-10 12:35:57 +0300

    Add Task-2 and .gitignore

---
+++ Task-2/ChatBot/ChatBotImitation.cs
@@ -0,0 +1,43 @@
+﻿void ChatBotImitation()
+{
+    string dateString;
+    var lastname = "";
+    var name = "";
+    DateTime date = default;
+    DateTime dateNow;
+    dateNow = DateTime.Now;
+
+    Console.Write("Hello, enter your lastname ");
+    lastname = Console.ReadLine();
+    while (!lastname.All(Char.IsLetter))
+    {
+        Console.Write("Please, enter correct lastname ");
+        lastname = Console.ReadLine();
+    }
+
+
+    Console.Write("Hello, enter your name ");
+    name = Console.ReadLine();
+    while (!name.All(Char.IsLetter))
+    {
+        Console.Write("Please, enter correct name ");
+        name = Console.ReadLine();
+    }
+
+    Console.Write("Enter desired date and time of the appointment \"yyyy mm dd hh:mm\": ");
+    dateString = Console.ReadLine();
+    date = DateTime.Parse(dateString);
+    while (dateNow.AddHours(1) > date)
+    {
+        Console.WriteLine("Enter correct date");
+        dateString = Console.ReadLine();
+        date = DateTime.Parse(dateString);
+        break;
+    }
+
+
+    Console.WriteLine($"{lastname} {name}, You are booked in for an appointment:");
+    Console.WriteLine($"{date:f}");
+}
+
+    ChatBotImitation();
