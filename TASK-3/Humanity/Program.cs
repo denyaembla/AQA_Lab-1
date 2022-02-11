cb16f1353c6dd4e59fba8d95a5799f8d55a0db1e
denyaembla <denisholder12@gmail.com>
2022-02-11 14:01:37 +0300

    TASK-3: Committed Program.cs

---
+++ TASK-3/Humanity/Program.cs
@@ -0,0 +1,13 @@
+﻿    
+    using Humanity;
+
+    Employee employee1 = new Employee("Dude", "Dudskii", "Engineer", 1000, "HornsAndHoofs",
+        "Russia", "Zelenograd", "Krasnoarmeyskaya");
+
+    Candidate candidate1 = new Candidate("Travis", "Barker", "Drummer", "at pop-punk band Blink-182", 1400);
+    
+    employee1.Display();
+    
+    candidate1.Display();
+    
+    
