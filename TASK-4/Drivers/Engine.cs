21ef682893fe14f57681f25dc4ed5487e41d1d87
denyaembla <denisholder12@gmail.com>
2022-02-15 16:32:47 +0300

    TASK-4: Implemented Engine class.

--- TASK-4/Drivers/Engine.cs
+++ TASK-4/Drivers/Engine.cs
@@ -1,9 +1,42 @@
+using System.Net;
+using Bogus;
+
 namespace Drivers;
 
 public class Engine
 {
-    private double Capacity;
-    private double Power;
-    private string FuelType;
+    
+
+    private int Capacity;
+    private int Power;
+
+    public enum fuelType
+    {
+        Diesel,
+        Gasoline
+    };
+ 
     private int MaxSpeed;
+    
+    public Engine(int capacity, int power, Enum fuelType, int maxSpeed)
+    {
+        Capacity = capacity;
+        Power = power;
+        fuelType = fuelType;
+        MaxSpeed = maxSpeed;
+    }
+
+    
+   
+
+    public static void CreateEngine()
+    {
+        
+        var engine = new Faker<Engine>()
+            .CustomInstantiator(faker => new Engine(
+                faker.Random.Int(min:50, max:250),
+                faker.Random.Int(min:70, max:160),
+                faker.PickRandom(fuelType),
+                faker.Random.Int(100, 220)));
+    }
 }
