fc11e732fb10de658cc996738c39647d9aaaaced
denyaembla <denisholder12@gmail.com>
2022-03-13 15:46:46 +0300

    T4: Added Program.cs

---
+++ Task-4/Task-4/Program.cs
@@ -0,0 +1,15 @@
+﻿using Task_4.Generators;
+using Task_4.ReportGenerators;
+
+CandidateGenerator.GenerateOneCandidate();
+UserFactory.GenerateUser();
+CandidateGenerator.GenerateAFewCandidates();
+CandidateReportGenerator.CandidateReport();
+
+Console.WriteLine("\n \n ");
+
+EmployeeGenerator.GenerateOneEmployee();
+UserFactory.GenerateUser();
+EmployeeGenerator.GenerateAFewEmployees();
+EmployeeReportGenerator.EmployeeReport();
+
