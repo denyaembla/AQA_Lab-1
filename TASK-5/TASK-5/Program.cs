354352d79db49a7eeba5fcbb43880dff20ed5998
denyaembla <denisholder12@gmail.com>
2022-03-27 19:25:35 +0300

    T5: Added sln., csproj. and Program.cs files

---
+++ TASK-5/TASK-5/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.ComponentModel.Design;
+using Task_5;
+using Task_5.Models;
+
+var result = Menu.GenerateFiveUsers();
+Menu.MainMenu(result);
+
+
+
+
+
+
+
+
